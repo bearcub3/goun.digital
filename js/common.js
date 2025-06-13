f0f6394b626cfe50cf89d8f4bf84bfa8f2c5789d
Go-Un(Emilie), Shin <emilieshin@gmail.com>
2025-06-13 19:54:43 +0100

    First commit for the website

---
+++ js/common.js
@@ -0,0 +1,151 @@
+(function (win, doc){
+    const website = {
+        init : function () {
+            this.goToTop();
+            this.textTransition();
+            this.screenSizeDetector();
+            this.location();
+        },
+        location : function(){
+            const path = win.location.pathname;
+            const wrapper = doc.querySelector('.wrapper');
+
+            win.addEventListener('load', (e) => {
+                let width = win.innerWidth;
+
+                if (width < 992) {
+                    console.log(path);
+                    path === '/ux/visualization.htm'?  wrapper.setAttribute("style", `background: no-repeat url("../imgs/Plutchik-wheel.png"); background-position-y: 11%; background-position-x: 160%;`) : wrapper.setAttribute("style", '');
+                } else if( width >= 992) {
+                    path === '/ux/visualization.htm'?  wrapper.setAttribute("style", `background: no-repeat url("../imgs/Plutchik-wheel.png"); background-position-y: 10%; background-position-x: 120%;`) : wrapper.setAttribute("style", '');
+                }
+            });
+
+            win.addEventListener('resize', (e) => {
+                let width = win.innerWidth;
+
+                if (width < 992) {
+                    console.log(path);
+                    path === '/ux/visualization.htm'?  wrapper.setAttribute("style", `background: no-repeat url("../imgs/Plutchik-wheel.png"); background-position-y: 11%; background-position-x: 160%;`) : wrapper.setAttribute("style", '');
+                } else if( width >= 992) {
+                    path === '/ux/visualization.htm'?  wrapper.setAttribute("style", `background: no-repeat url("../imgs/Plutchik-wheel.png"); background-position-y: 10%; background-position-x: 120%;`) : wrapper.setAttribute("style", '');
+                }
+            });
+
+        },
+        textTransition : function () {
+            const links = doc.querySelectorAll('.option-link');
+
+            links.forEach((el, i) => {
+                // innerText will change when mouseover
+                el.addEventListener('mouseover', (e) => {
+                    e.preventDefault();
+                    if (i === 0){
+                        el.textContent = `Retrospective Design`;
+                    } else if (i === 1){
+                        el.textContent = `Progressive Design`;
+                    } else {
+                        el.textContent = `User Research`;
+                    }
+                    
+                });
+
+                el.addEventListener('mouseout', (e) => {
+                    e.preventDefault();
+                    if (i === 0){
+                        el.textContent = `I already have a website.`;
+                    } else if (i === 1){
+                        el.textContent = `I don't have a website.`;
+                    } else {
+                        el.textContent = `What the heck is accessibility?`;
+                    }
+                    
+                });
+            });
+        },
+        goToTop: function() {
+            const top = doc.querySelector('.top-btn');
+            
+            win.addEventListener('scroll', (e) => {
+                let scrollY = win.scrollY;
+                if (scrollY > 600) {
+                    top.classList.add('visible');
+                    top.classList.remove('no-visible'); 
+                } else {
+                    top.classList.remove('visible');
+                    top.classList.add('no-visible');
+                }
+            })
+        },
+        screenSizeDetector: function() {
+            // Main navigation realignment
+            const nav = doc.querySelector('nav > ul');
+
+            // Visualization Graph visibility
+            const columns = doc.querySelectorAll('.col-1');
+            const methodologySection = doc.querySelectorAll('#methodology ~ *');
+
+            // subpage navigation
+            const subnav = doc.querySelector('.subpage-nav');
+
+
+            win.addEventListener('load', (e) => {
+                let width = win.innerWidth;
+
+                if (width < 992) {
+                    subnav.classList.add('no-visible');
+
+                    columns.forEach((col) => {
+                        col.classList.add('no-visible');
+                    });
+                    
+                    methodologySection.forEach((col) => {
+                        col.classList.remove('second-column');
+                    });
+
+                } else if( width >= 992) {
+                    subnav.classList.remove('no-visible');
+                    subnav.classList.add('visible');
+
+                    nav.classList.remove('column');
+                    columns.forEach((col) => {
+                        col.classList.add('visible');
+                    });
+                }
+            });
+
+
+            win.addEventListener('resize', (e) => {
+                let width = win.innerWidth;
+
+                if(width < 992){
+                    subnav.classList.add('no-visible');
+                    nav.classList.add('column');
+                    columns.forEach((col) => {
+                        col.classList.remove('visible');
+                        col.classList.add('no-visible');
+                    });
+
+                    methodologySection.forEach((col) => {
+                        col.classList.remove('second-column');
+                    });
+                    
+                } else if( width >= 992){
+                    subnav.classList.remove('no-visible');
+                    subnav.classList.add('visible');
+                    
+                    nav.classList.remove('column');
+                    columns.forEach((col) => {
+                        col.classList.remove('no-visible');
+                        col.classList.add('visible');
+                    });
+
+                    methodologySection.forEach((col) => {
+                        col.classList.add('second-column');
+                    });
+                }
+            })
+        }
+    }
+    website.init();
+})(window, document);
---
+++ js/menu.js
@@ -0,0 +1,160 @@
+(function (win, doc){
+    const website = {
+        init : function () {
+            // this.textTransition();
+            this.dropdown();
+            // this.goToTop();
+            this.screenSizeDetector();
+        },
+        dropdown : function() {
+            // submenu dropdown
+            const menus = doc.querySelectorAll('.submenu > a');
+
+            // Main section position alignment
+            const main = doc.querySelector('main');
+            const header = doc.querySelector('header');
+
+            menus.forEach((menu, i) => {
+                menu.addEventListener('focus', (e) => {
+                    e.preventDefault();
+
+                    const submenu = menu.nextElementSibling;
+
+                    if (submenu && submenu.classList.contains('no-visible')){
+                        submenu.classList.remove('no-visible');
+                        submenu.classList.add('visible');
+                    }
+
+                    // Main section position alignment depending on the menu height
+                    let headerHeight = header.offsetHeight;
+                    main.style.marginTop = `${headerHeight}px`;
+                    
+                });
+            })
+
+            // when the logo is in the focus state
+            const logo = doc.querySelector('.logo a');
+
+            logo.addEventListener('focus', (e) => { 
+                e.preventDefault();
+
+                const firstMenu = doc.querySelector('.submenu:first-child ul');
+
+                if (firstMenu && firstMenu.classList.contains('visible')){
+                    firstMenu.classList.remove('visible');
+                    firstMenu.classList.add('no-visible');
+                }
+
+                // Main section position alignment depending on the menu height
+                let headerHeight = header.offsetHeight;
+                main.style.marginTop = `${headerHeight}px`;
+            })
+
+            // dropdown menu
+            const dropdowns = doc.querySelectorAll('.dropdown');
+
+            dropdowns.forEach((el, i) => {
+                el.addEventListener('focus', (e) => {
+                    e.preventDefault();
+                    const parent = el.parentElement.parentElement;
+
+                    if(parent && parent.classList.contains('no-visible')){
+                        parent.classList.remove('no-visible');
+                        parent.classList.add('visible');
+                        parent.classList.add('selected');
+                        console.log(parent);
+                        
+                    }
+
+                    // Main section position alignment depending on the menu height
+                    let headerHeight = header.offsetHeight;
+                    main.style.marginTop = `${headerHeight}px`;
+                })
+            })
+
+            dropdowns.forEach((el, i) => {
+                el.addEventListener('focusout', (e) => {
+                    e.preventDefault();
+                    const parent = el.parentElement.parentElement;
+
+                    if(i === 3 && parent.classList.contains('visible')){
+                        parent.classList.remove('visible');
+                        parent.classList.add('no-visible');
+                    }
+
+                    // Main section position alignment depending on the menu height
+                    let headerHeight = header.offsetHeight;
+                    main.style.marginTop = `${headerHeight}px`;
+                })  
+            })
+        },
+
+        screenSizeDetector: function() {
+            // Main navigation realignment
+            const nav = doc.querySelector('nav > ul');
+            const submenu = nav.nextElementSibling;
+
+            // Main section position alignment
+            const main = doc.querySelector('main');
+            const header = doc.querySelector('header');
+
+            // Sub page navigation
+            const subnav = doc.querySelector('.subpage-nav');
+            
+            // sub page layout
+            const titleArea = doc.querySelector('.title-area');
+            const contentArea = doc.querySelector('.content-area');
+
+            win.addEventListener('resize', (e) => {
+                let width = win.innerWidth;
+                let headerHeight = header.offsetHeight;
+
+                // Main section position alignment depending on the menu height
+                main.style.marginTop = `${headerHeight}px`;
+
+                if(width < 992){
+                    nav.classList.add('column');
+                    
+                    // subpage layout changes
+                    if(subnav.classList.contains('visible')) {
+                        subnav.classList.remove('visible');
+
+                    } else {
+                        subnav.classList.add('no-visible');
+                    }
+
+                    // subnav.classList.contains('visible')? subnav.classList.remove('visible') : 
+
+                } else if( width >= 992){
+                    nav.classList.remove('column');
+
+                    // subpage layout changes
+                    subnav.classList.contains('no-visible')? subnav.classList.remove('no-visible') : subnav.classList.add('visible');
+
+                }
+            })
+
+            win.addEventListener('load', (e) => {
+                let width = win.innerWidth;
+                let headerHeight = header.offsetHeight;
+
+                // Main section position alignment depending on the menu height
+                main.style.marginTop = `${headerHeight}px`;
+
+                if (width < 992) {
+                    nav.classList.add('column');
+
+                    // subpage layout changes
+                    subnav.classList.contains('visible')? subnav.classList.remove('visible') : subnav.classList.add('no-visible');
+
+                } else if( width >= 992) {
+                    nav.classList.remove('column');
+
+                    // subpage layout changes
+                    subnav.classList.contains('no-visible')? subnav.classList.remove('no-visible') : subnav.classList.add('visible');
+                }
+            });
+        }
+    }
+    website.init();
+})(window, document);
